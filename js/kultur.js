bdc45facf7e2722eb421c83bdafb0980183ba7f1
Mihaela Stavila <ms@inleadmedia.com>
2017-08-08 16:29:38 +0300

    EASYOPAC-563 - Added style for kultur-midbib subtheme.

--- js/kultur.js
+++ js/kultur.js
@@ -1,4 +1,14 @@
 (function ($) {
   'use strict';
 
+  function updateSubmenu() {
+    var secondaryMenu = $('.secondary-menu-wrapper');
+    $('.site-name-wrapper').after(secondaryMenu);
+    $('<div class="transform"></div>' ).insertBefore('.secondary-menu');
+  }
+
+ $(document).ready(function() {
+    updateSubmenu();
+
+  });
 })(jQuery);
--- template.php
+++ template.php
@@ -40,3 +40,7 @@ function kultur_subtheme_preprocess_html(&$vars) {
 
   drupal_add_html_head($apple_touch_icon, 'apple_touch_icon');
 }
+
+function kultur_subtheme_menu_tree__sub_menu(&$vars) {
+  var_dump($vars);
+}
